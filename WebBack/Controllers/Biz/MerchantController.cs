ed39f9ea0977aedde614d99031fd3caf77bd9394
qiuqingwen <qqw@uplink.net.cn>
2018-08-22 15:17:57 +0800

    .

--- WebBack/Controllers/Biz/MerchantController.cs
+++ WebBack/Controllers/Biz/MerchantController.cs
@@ -36,6 +36,10 @@ namespace WebBack.Controllers.Biz
             return View(model);
         }
 
+        public ViewResult MachineListByBindable()
+        {
+            return View();
+        }
 
         public CustomJsonResult GetList(SearchCondition condition)
         {
@@ -91,5 +95,66 @@ namespace WebBack.Controllers.Biz
             return BizFactory.Merchant.Edit(this.CurrentUserId, model.SysMerchatUser, model.Merchant);
         }
 
+
+        public CustomJsonResult GetMachineListByBinded(WebBack.Models.Biz.MerchantMachine.SearchCondition condition)
+        {
+
+            string deviceId = condition.DeviceId.ToSearchString();
+
+            var list = (from mp in CurrentDb.MerchantMachine
+                        join m in CurrentDb.Merchant on mp.MerchantId equals m.Id
+                        join p in CurrentDb.Machine on mp.MachineId equals p.Id
+                        where
+                        p.IsUse == true &&
+                        mp.Status == Enumeration.MerchantMachineStatus.Bind &&
+                               (deviceId.Length == 0 || p.DeviceId.Contains(deviceId))
+
+                        select new { mp.Id, mp.MerchantId, mp.MachineId, MerchantName = m.Name, p.DeviceId, MachineName = p.Name, p.MacAddress });
+
+            int total = list.Count();
+
+            int pageIndex = condition.PageIndex;
+            int pageSize = 10;
+            list = list.OrderBy(r => r.Id).Skip(pageSize * (pageIndex)).Take(pageSize);
+
+            PageEntity pageEntity = new PageEntity { PageSize = pageSize, TotalRecord = total, Rows = list };
+
+            return Json(ResultType.Success, pageEntity);
+        }
+
+
+        public CustomJsonResult GetMachineListByBindable(WebBack.Models.Biz.MerchantMachine.SearchCondition condition)
+        {
+
+            string deviceId = condition.DeviceId.ToSearchString();
+
+            var list = (from u in CurrentDb.Machine
+                        where u.IsUse == false &&
+                               (deviceId.Length == 0 || u.DeviceId.Contains(deviceId))
+                        select new { u.Id, u.Name, u.DeviceId, u.MacAddress, u.CreateTime });
+
+            int total = list.Count();
+
+            int pageIndex = condition.PageIndex;
+            int pageSize = 10;
+            list = list.OrderBy(r => r.Id).Skip(pageSize * (pageIndex)).Take(pageSize);
+
+            PageEntity pageEntity = new PageEntity { PageSize = pageSize, TotalRecord = total, Rows = list };
+
+            return Json(ResultType.Success, pageEntity);
+        }
+
+        [HttpPost]
+        public CustomJsonResult BindOffMachine(string merchantId, string machineId)
+        {
+            return BizFactory.MerchantMachine.BindOff(this.CurrentUserId, merchantId, machineId);
+        }
+
+
+        [HttpPost]
+        public CustomJsonResult BindOnMachine(string merchantId, string machineId)
+        {
+            return BizFactory.MerchantMachine.BindOn(this.CurrentUserId, merchantId, machineId);
+        }
     }
 }
