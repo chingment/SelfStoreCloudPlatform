a3818945eeab2882f0b2326cc337bcea4f3a23f8
qiuqingwen <qqw@uplink.net.cn>
2018-08-27 12:26:38 +0800

    .

---
+++ WebMerch/Controllers/CompanyController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAgent.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public ActionResult ListBySupplier()
+        {
+            return View();
+        }
+    }
+}
