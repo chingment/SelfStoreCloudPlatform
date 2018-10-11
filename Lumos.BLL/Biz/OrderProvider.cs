3e59a90c2685b534c28d40b226e9c882e5811324
qiuqingwen <qqw@uplink.net.cn>
2018-10-11 14:54:09 +0800

    .

--- Lumos.BLL/Biz/OrderProvider.cs
+++ Lumos.BLL/Biz/OrderProvider.cs
@@ -165,7 +165,7 @@ namespace Lumos.BLL
                     return new CustomJsonResult(ResultType.Success, ResultCode.Success, "该订单已经取消");
                 }
 
-                if (order.Status != Enumeration.OrderStatus.Payed || order.Status != Enumeration.OrderStatus.Completed)
+                if (order.Status != Enumeration.OrderStatus.Payed && order.Status != Enumeration.OrderStatus.Completed)
                 {
                     order.Status = Enumeration.OrderStatus.Cancled;
                     order.Mender = GuidUtil.Empty();
