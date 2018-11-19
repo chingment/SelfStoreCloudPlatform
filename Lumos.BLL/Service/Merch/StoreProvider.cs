f3925ac9d0298464e998373f6abe680bd7ef6be2
qiuqingwen <qqw@uplink.net.cn>
2018-11-19 18:19:10 +0800

    .

---
+++ Lumos.BLL/Service/Merch/StoreProvider.cs
@@ -0,0 +1,185 @@
+using Lumos.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Lumos.BLL.Service.Merch
+{
+    public class StoreProvider : BaseProvider
+    {
+        public CustomJsonResult GetDetails(string pOperater, string pMerchantId, string pStoreId)
+        {
+            var ret = new RetStoreGetDetails();
+            var store = CurrentDb.Store.Where(m => m.MerchantId == pMerchantId && m.Id == pStoreId).FirstOrDefault();
+            if (store != null)
+            {
+                ret.StoreId = store.Id ?? "";
+                ret.Name = store.Name ?? "";
+                ret.Address = store.Address ?? "";
+                ret.Description = store.Description ?? "";
+                ret.Status = store.Status;
+            }
+
+            return new CustomJsonResult(ResultType.Success, ResultCode.Success, "获取成功", ret);
+        }
+
+        public CustomJsonResult Add(string pOperater, string pMerchantId, RopStoreAdd rop)
+        {
+            CustomJsonResult result = new CustomJsonResult();
+            using (TransactionScope ts = new TransactionScope())
+            {
+                var isExistStore = CurrentDb.Store.Where(m => m.MerchantId == pMerchantId && m.Name == rop.Name).FirstOrDefault();
+                if (isExistStore != null)
+                {
+                    return new CustomJsonResult(ResultType.Failure, ResultCode.Failure, "名称已存在,请使用其它");
+                }
+
+                var store = new Store();
+                store.Id = GuidUtil.New();
+                store.MerchantId = pMerchantId;
+                store.Name = rop.Name;
+                store.Address = rop.Address;
+                store.Description = rop.Description;
+                store.Status = Enumeration.StoreStatus.Closed;
+                store.CreateTime = this.DateTime;
+                store.Creator = pOperater;
+                CurrentDb.Store.Add(store);
+                CurrentDb.SaveChanges();
+
+                ts.Complete();
+                result = new CustomJsonResult(ResultType.Success, ResultCode.Success, "添加成功");
+
+            }
+            return result;
+        }
+
+        public CustomJsonResult Edit(string pOperater, string pMerchantId, RopStoreEdit rop)
+        {
+            CustomJsonResult result = new CustomJsonResult();
+            using (TransactionScope ts = new TransactionScope())
+            {
+
+                var isExistStore = CurrentDb.Store.Where(m => m.MerchantId == pMerchantId && m.Id != rop.StoreId && m.Name == rop.Name).FirstOrDefault();
+                if (isExistStore != null)
+                {
+                    return new CustomJsonResult(ResultType.Failure, ResultCode.Failure, "名称已存在,请使用其它");
+                }
+
+                var store = CurrentDb.Store.Where(m => m.Id == rop.StoreId).FirstOrDefault();
+
+                if (rop.Status == Enumeration.StoreStatus.Opened)
+                {
+                    var storeMachineBindCount = CurrentDb.StoreMachine.Where(m => m.StoreId == rop.StoreId && m.IsBind == true).Count();
+                    if (storeMachineBindCount == 0)
+                    {
+                        return new CustomJsonResult(ResultType.Failure, ResultCode.Failure, "设置为正常状态，必须先在机器管理里绑定一台机器");
+                    }
+                }
+
+                store.Name = rop.Name;
+                store.Address = rop.Address;
+                store.Status = rop.Status;
+                store.MendTime = this.DateTime;
+                store.Mender = pOperater;
+                CurrentDb.SaveChanges();
+                ts.Complete();
+                result = new CustomJsonResult(ResultType.Success, ResultCode.Success, "保存成功");
+            }
+            return result;
+        }
+
+        public CustomJsonResult BindOnMachine(string pOperater, string pStoreId, string[] pMachineIds)
+        {
+            CustomJsonResult result = new CustomJsonResult();
+            using (TransactionScope ts = new TransactionScope())
+            {
+                if (pMachineIds == null)
+                {
+                    return new CustomJsonResult(ResultType.Failure, ResultCode.Failure, "没有机器人");
+                }
+
+                if (pMachineIds.Length == 0)
+                {
+                    return new CustomJsonResult(ResultType.Failure, ResultCode.Failure, "没有机器人");
+                }
+
+                var store = CurrentDb.Store.Where(m => m.Id == pStoreId).FirstOrDefault();
+
+                if (store == null)
+                {
+                    return new CustomJsonResult(ResultType.Failure, ResultCode.Failure, "没有便利店");
+                }
+
+                foreach (var machineId in pMachineIds)
+                {
+                    var storeMachine = CurrentDb.StoreMachine.Where(m => m.StoreId == pStoreId && m.MachineId == machineId).FirstOrDefault();
+                    if (storeMachine == null)
+                    {
+                        storeMachine = new StoreMachine();
+                        storeMachine.Id = GuidUtil.New();
+                        storeMachine.MerchantId = store.MerchantId;
+                        storeMachine.MachineId = machineId;
+                        storeMachine.StoreId = pStoreId;
+                        storeMachine.IsBind = true;
+                        storeMachine.CreateTime = this.DateTime;
+                        storeMachine.Creator = pOperater;
+                        CurrentDb.StoreMachine.Add(storeMachine);
+                        CurrentDb.SaveChanges();
+                    }
+                    else
+                    {
+                        var machine = CurrentDb.Machine.Where(m => m.Id == machineId).FirstOrDefault();
+                        storeMachine.MachineName = machine.Name;
+                        storeMachine.IsBind = true;
+                        storeMachine.MendTime = this.DateTime;
+                        storeMachine.Mender = pOperater;
+                        CurrentDb.SaveChanges();
+                    }
+                }
+
+                ts.Complete();
+                result = new CustomJsonResult(ResultType.Success, ResultCode.Success, "绑定成功");
+            }
+            return result;
+        }
+
+        public CustomJsonResult BindOffMachine(string pOperater, string pStoreId, string pMachineId)
+        {
+            CustomJsonResult result = new CustomJsonResult();
+            using (TransactionScope ts = new TransactionScope())
+            {
+                var lStoreMachines = CurrentDb.StoreMachine.Where(m => m.StoreId == pStoreId).ToList();
+
+                foreach (var item in lStoreMachines)
+                {
+                    if (item.MachineId == pMachineId)
+                    {
+                        item.IsBind = false;
+                        item.MendTime = this.DateTime;
+                        item.Mender = pOperater;
+                    }
+                }
+
+                var storeMachineBindCount = lStoreMachines.Where(m => m.IsBind == true).Count();
+                if (storeMachineBindCount == 0)
+                {
+                    var store = CurrentDb.Store.Where(m => m.Id == pStoreId).FirstOrDefault();
+
+                    store.Status = Enumeration.StoreStatus.Closed;
+                    store.MendTime = this.DateTime;
+                    store.Mender = pOperater;
+                }
+
+                CurrentDb.SaveChanges();
+                ts.Complete();
+                result = new CustomJsonResult(ResultType.Success, ResultCode.Success, "解绑成功");
+            }
+            return result;
+        }
+
+
+    }
+}
---
+++ Lumos/CustomJsonResult/CustomJsonResult.cs
@@ -0,0 +1,293 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lumos
+{
+    #region CustomJsonResult 自定义JsonResult
+    /// <summary>
+    /// 扩展JsonResult
+    /// </summary>
+    /// 
+
+
+
+    public class CustomJsonResult<T> : ActionResult, IResult<T>
+    {
+        private ResultType _result = ResultType.Unknown;
+        private ResultCode _code = ResultCode.Unknown;
+        private string _message = "";
+        private T _data;
+
+        /// <summary>
+        /// 结果状态默认为Unknown
+        /// </summary>
+        public ResultType Result
+        {
+            get
+            {
+                return _result;
+            }
+            set
+            {
+                _result = value;
+            }
+        }
+
+        /// <summary>
+        /// 信息默认返回空字符串
+        /// </summary>
+        public string Message
+        {
+            get
+            {
+                return _message;
+            }
+            set
+            {
+                _message = value;
+            }
+        }
+
+        /// <summary>
+        /// 信息默认返回空字符串
+        /// </summary>
+        public ResultCode Code
+        {
+            get
+            {
+                return _code;
+            }
+            set
+            {
+                _code = value;
+            }
+        }
+
+        /// <summary>
+        /// 内容默认为null
+        /// </summary>
+        public T Data
+        {
+            get
+            {
+                return _data;
+            }
+            set
+            {
+                _data = value;
+            }
+        }
+
+        public JsonConverter[] JsonConverter
+        {
+            get;
+            set;
+        }
+
+        public JsonSerializerSettings JsonSerializerSettings
+        {
+            get;
+            set;
+        }
+
+        public CustomJsonResult()
+        {
+            this.JsonSerializerSettings = new JsonSerializerSettings();
+        }
+
+        private void SetCustomJsonResult(string contenttype, ResultType type, ResultCode code, string message, T data, JsonSerializerSettings settings, params JsonConverter[] converters)
+        {
+            //this.ContentType = contenttype;
+            this._result = type;
+            this._code = code;
+            this._message = message;
+            this._data = data;
+            this.JsonSerializerSettings = settings;
+            this.JsonConverter = converters;
+        }
+
+        public CustomJsonResult(string contenttype, ResultType type, ResultCode code, string message, T content, JsonSerializerSettings settings, params JsonConverter[] converters)
+        {
+            SetCustomJsonResult(contenttype, type, code, message, content, settings, converters);
+        }
+
+        public CustomJsonResult(string contenttype, ResultType type, string message, T content, JsonSerializerSettings settings, params JsonConverter[] converters)
+        {
+            SetCustomJsonResult(contenttype, type, ResultCode.Unknown, message, content, settings, converters);
+        }
+
+        public CustomJsonResult(string contenttype, ResultType type, string message, T content, params JsonConverter[] converters)
+        {
+            SetCustomJsonResult(contenttype, type, ResultCode.Unknown, message, content, null, converters);
+        }
+
+        public CustomJsonResult(string contenttype, ResultType type, string message, T content, JsonSerializerSettings settings)
+        {
+            SetCustomJsonResult(contenttype, type, ResultCode.Unknown, message, content, settings);
+        }
+
+        public CustomJsonResult(ResultType type, ResultCode code, string message, T content, params JsonConverter[] converters)
+        {
+            SetCustomJsonResult(null, type, code, message, content, null, converters);
+        }
+
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentException("context");
+            }
+
+            HttpResponseBase response = context.HttpContext.Response;
+
+            //if (!String.IsNullOrEmpty(ContentType))
+            //{
+            //    response.ContentType = ContentType;
+            //}
+            //else
+            //{
+            //    response.ContentType = "application/json";
+            //}
+
+            //if (ContentEncoding != null)
+            //{
+            //    response.ContentEncoding = ContentEncoding;
+            //}
+
+            response.Write(GetResultJson());
+
+        }
+
+        public override string ToString()
+        {
+            return GetResultJson();
+        }
+
+
+        public string GetResultJson()
+        {
+            StringBuilder json = new StringBuilder();
+            json.Append("{");
+
+            try
+            {
+                json.Append("\"result\": " + (int)this._result + ",");
+
+                if (this._data != null)
+                {
+                    if (this._data is string)
+                    {
+                        if (!string.IsNullOrWhiteSpace(this._data.ToString()))
+                        {
+                            json.Append("\"data\":" + this._data + ",");
+                        }
+                    }
+                    else
+                    {
+
+                        if (this.JsonSerializerSettings == null)
+                        {
+                            this.JsonSerializerSettings = new JsonSerializerSettings() { ContractResolver = new NullToEmptyStringResolver() };
+
+                        }
+
+                        JsonConvert.DefaultSettings = new Func<JsonSerializerSettings>(() =>
+                        {
+                            //日期类型默认格式化处理
+                            this.JsonSerializerSettings.DateFormatHandling = Newtonsoft.Json.DateFormatHandling.MicrosoftDateFormat;
+                            this.JsonSerializerSettings.DateFormatString = "yyyy-MM-dd HH:mm:ss";
+                            return this.JsonSerializerSettings;
+                        });
+                        this.JsonSerializerSettings.Converters = this.JsonConverter;
+                        this.JsonSerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                        json.Append("\"data\":" + JsonConvert.SerializeObject(this._data, Formatting.None, this.JsonSerializerSettings) + ",");
+                    }
+                }
+
+                if (this._code != ResultCode.Unknown)
+                {
+                    json.Append("\"code\": \"" + ((int)this._code).ToString() + "\",");
+                }
+
+
+                json.Append("\"message\":" + JsonConvert.SerializeObject(this._message) + "");
+
+
+            }
+            catch (Exception ex)
+            {
+
+                json.Append("\"result\": " + (int)ResultType.Exception + ",");
+                json.Append("\"message\":\"" + string.Format("CustomJsonResult转换发生异常:{0}", ex.Message) + "\"");
+                //转换失败记录日志
+            }
+            json.Append("}");
+
+            string s = json.ToString();
+
+            return s;
+        }
+    }
+
+    public class CustomJsonResult : CustomJsonResult<object>, IResult
+    {
+
+        public CustomJsonResult()
+        {
+
+        }
+
+        public CustomJsonResult(ResultType type, string message) : base(type, ResultCode.Unknown, message, null, null)
+        {
+
+        }
+
+        public CustomJsonResult(ResultType type, ResultCode code, string message) : base(type, code, message, null, null)
+        {
+
+        }
+
+        public CustomJsonResult(ResultType type, ResultCode code, string message, object content) : base(type, code, message, content, null)
+        {
+
+        }
+
+        public CustomJsonResult(ResultType type, ResultCode code, string message, object content, params JsonConverter[] converters) : base(type, code, message, content, converters)
+        {
+
+        }
+
+        public CustomJsonResult(string contenttype, ResultType type, ResultCode code, string message, object content, JsonSerializerSettings settings, params JsonConverter[] converters) : base(contenttype, type, code, message, content, settings, converters)
+        {
+
+        }
+
+        public CustomJsonResult(string contenttype, ResultType type, string message, object content, JsonSerializerSettings settings, params JsonConverter[] converters) : base(contenttype, type, message, content, settings, converters)
+        {
+
+        }
+
+        public CustomJsonResult(string contenttype, ResultType type, string message, object content, params JsonConverter[] converters) : base(contenttype, type, message, content, converters)
+        {
+
+        }
+
+        public CustomJsonResult(string contenttype, ResultType type, string message, object content, JsonSerializerSettings settings) : base(contenttype, type, message, content, settings)
+        {
+
+        }
+
+
+    }
+
+    #endregion
+}
