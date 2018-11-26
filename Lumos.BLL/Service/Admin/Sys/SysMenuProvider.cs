6136034d2dc29a032d58f5c2df0132a43b2af6fe
qiuqingwen <qqw@uplink.net.cn>
2018-11-26 11:52:55 +0800

    .

--- Lumos.BLL/Service/Admin/Sys/SysMenuProvider.cs
+++ Lumos.BLL/Service/Admin/Sys/SysMenuProvider.cs
@@ -49,7 +49,18 @@ namespace Lumos.BLL.Service.Admin
             return new CustomJsonResult(ResultType.Success, ResultCode.Success, "操作成功", ret);
         }
 
-        public static IEnumerable<SysMenu> GetFatherList(IList<SysMenu> list, string pId)
+
+        private List<SysMenu> GetFathers(Enumeration.BelongSite belongSite, string id)
+        {
+            var sysMenus = CurrentDb.SysMenu.Where(m => m.BelongSite == belongSite).ToList();
+
+            var list = new List<SysMenu>();
+            var list2 = list.Concat(GetFatherList(sysMenus, id));
+            return list2.ToList();
+        }
+
+
+        public IEnumerable<SysMenu> GetFatherList(IList<SysMenu> list, string pId)
         {
             var query = list.Where(p => p.Id == pId).ToList();
             return query.ToList().Concat(query.ToList().SelectMany(t => GetFatherList(list, t.PId)));
@@ -74,10 +85,9 @@ namespace Lumos.BLL.Service.Admin
             CustomJsonResult result = new CustomJsonResult();
             using (TransactionScope ts = new TransactionScope())
             {
-                var sysMenus = CurrentDb.SysMenu.Where(m => m.BelongSite == rop.BelongSite).ToList();
-                var sysMenusGetFather = GetFatherList(sysMenus, rop.PId).ToList();
-                int dept = sysMenusGetFather.Count;
-                var isExists = sysMenus.Where(m => m.PId == rop.PId && m.Name == rop.Name && m.Dept == dept).FirstOrDefault();
+                var fathter = GetFathers(rop.BelongSite, rop.PId);
+                int dept = fathter.Count;
+                var isExists = CurrentDb.SysMenu.Where(m => m.PId == rop.PId && m.Name == rop.Name && m.Dept == dept).FirstOrDefault();
                 if (isExists != null)
                 {
                     return new CustomJsonResult(ResultType.Failure, "该名称在同一级别已经存在");
@@ -127,14 +137,15 @@ namespace Lumos.BLL.Service.Admin
                     return new CustomJsonResult(ResultType.Failure, ResultCode.Failure, "数据为空");
                 }
 
-                var sysMenus = CurrentDb.SysMenu.Where(m => m.BelongSite == sysMenu.BelongSite).ToList();
-                var sysMenusGetFather = GetFatherList(sysMenus, sysMenu.PId).ToList();
-                int dept = sysMenusGetFather.Count;
-                var isExists = sysMenus.Where(m => m.PId == sysMenu.PId && m.Name == rop.Name && m.Id != rop.Id).FirstOrDefault();
+                var fathter = GetFathers(sysMenu.BelongSite, sysMenu.PId);
+                int dept = fathter.Count;
+                var isExists = CurrentDb.SysMenu.Where(m => m.PId == sysMenu.PId && m.Name == rop.Name && m.Dept == dept && m.Id != rop.Id).FirstOrDefault();
                 if (isExists != null)
                 {
                     return new CustomJsonResult(ResultType.Failure, ResultCode.Failure, string.Format("保存失败，该名称({0})已被同一级别使用", rop.Name));
                 }
+
+
 
                 sysMenu.Name = rop.Name;
                 sysMenu.Url = rop.Url;
