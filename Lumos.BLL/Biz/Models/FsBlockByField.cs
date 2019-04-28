c51fa425bad694557859184f218d1ae44333c3e6
jiecong <jiecong@outlook.com>
2019-04-28 11:15:37 +0800

    .

---
+++ Lumos.BLL/Biz/Models/FsBlockByField.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lumos.BLL
+{
+    public class FsBlockByField
+    {
+        public FsBlockByField()
+        {
+            this.Tag = new FsTag();
+            this.Data = new List<FsField>();
+        }
+
+        public FsTag Tag { get; set; }
+        public List<FsField> Data { get; set; }
+    }
+}
--- Lumos.BLL/Service/ApiApp/RModels/RetOrderDetails.cs
+++ Lumos.BLL/Service/ApiApp/RModels/RetOrderDetails.cs
@@ -8,6 +8,11 @@ namespace Lumos.BLL.Service.ApiApp
 {
     public class RetOrderDetails
     {
+        public string Id { get; set; }
+        public string Sn { get; set; }
+        public string Status { get; set; }
+        public string StatusName { get; set; }
         public List<FsBlock> Blocks { get; set; }
+        public List<FsBlockByField> FieldBlocks { get; set; }
     }
 }
